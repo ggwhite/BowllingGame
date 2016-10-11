8dd4011c3c3b3c2e4cadbb940554379693fa0a12
WhiteChang <VDWhiteChang@taiwanmobile.com>
2016-10-11 23:11:38 +0800

    done

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,17 +1,213 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
 
+	private static final int STRIKE = 10;
+	private static final int PADDING = 0;
+	private static final int UNROLL = -99;
+	
+	private List<Frame> frames = new ArrayList<Frame>();
+	
+	public Game() {}
 
-	public Game() {
-		
-	}
-
+	/**
+	 * 丟球
+	 * @param int 擊倒球瓶
+	 */
 	public void roll(int pins) {
 		
+		if (!isPlayable()) {
+			return;
+		}
+		
+		Frame frame = getCurrentFrame();
+		
+		if (frame.isOver()) {
+			frame = addEmptyFrame();
+		}
+		
+		frame.roll(pins);
 	}
 
+	/**
+	 * 取得當前分數
+	 * 該局未結束不會計算
+	 */
 	public int sum() {
-		return 0;
+		int scoure = 0;
+		
+		for (Frame frame : frames) {
+			
+			int index = frames.indexOf(frame);
+		
+			if (!frame.isOver()) {
+				break;
+			}
+			
+			if (Game.UNROLL != frame.getBlock1() && Game.PADDING != frame.getBlock1()) {
+				scoure += frame.getBlock1();
+			}
+			if (Game.UNROLL != frame.getBlock2() && Game.PADDING != frame.getBlock2()) {
+				scoure += frame.getBlock2();
+			}
+			
+			if (frame instanceof LastFrame) {
+				if (Game.UNROLL != ((LastFrame) frame).getBlock3() && Game.PADDING != ((LastFrame) frame).getBlock3()) {
+					scoure += ((LastFrame) frame).getBlock3();
+				}
+			}
+			
+			
+			//判斷加分區
+			if (index <= 0) {
+				continue;
+			}
+			//取得上一個Frame
+			Frame preFrame = frames.get(index - 1);
+			if (Game.STRIKE == preFrame.getBlock1()) {
+				scoure += frame.getBlock1();
+				scoure += frame.getBlock2();
+				
+				//再上一個Frame
+				if (index > 1) {
+					Frame beforePreFrame = frames.get(index - 2);
+					if (Game.STRIKE == beforePreFrame.getBlock1()) {
+						scoure += frame.getBlock1();
+					}
+				}
+			} else if (preFrame.getBlock1() + preFrame.getBlock2() == 10) {
+				scoure += frame.getBlock1();
+			}
+		}
+		
+		return scoure;
+	}
+	
+	
+	private class Frame {
+		protected int block1 = Game.UNROLL;
+		protected int block2 = Game.UNROLL;
+		
+		public int getBlock1() {
+			return block1;
+		}
+		
+		public int getBlock2() {
+			return block2;
+		}
+		
+		public boolean isOver() {
+			if (Game.UNROLL == block1 || Game.UNROLL == block2) {
+				return false;
+			} else if (block1 >= 0 && block2 >= 0) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+		
+		public void roll(int pins) {
+			if (Game.UNROLL == block1) {
+				block1 = pins;
+				if (Game.STRIKE == pins) {
+					block2 = Game.PADDING;
+				}
+			} else if (Game.UNROLL == block2) {
+				block2 = pins;
+			}
+		}
+		
+		public String toString() {
+			return "Frame[1: " + Integer.toString(block1) + 
+					", 2: " + Integer.toString(block2) + 
+					", isOver = " + isOver() + "]";
+		}
+	}
+	
+	private class LastFrame extends Frame {
+		
+		private int block3 = Game.UNROLL;
+		
+		public int getBlock3() {
+			return block3;
+		}
+		
+		@Override
+		public boolean isOver() {
+			if (Game.UNROLL == block1 || Game.UNROLL == block2) {
+				return false;
+			} else if (block1 >= 0 && block2 >= 0 && (block1 + block2 < 10 || block3 >= 0)) {
+				return true;
+			}
+			return false;
+		}
+		
+		@Override
+		public void roll(int pins) {
+			if (Game.UNROLL == block1) {
+				block1 = pins;
+			} else if (Game.UNROLL == block2) {
+				block2 = pins;
+			} else if (block1 + block2 >= 10 && Game.UNROLL == block3) {
+				block3 = pins;
+			}
+		}
+		
+		public String toString() {
+			return "Frame[1: " + Integer.toString(block1) + 
+					", 2: " + Integer.toString(block2) + 
+					", 3: " + Integer.toString(block3) + 
+					", isOver = " + isOver() + "]";
+		}
+	}
+	
+	/**
+	 * 新增空局
+	 * 若是第10局則使用LastFrame物件
+	 */
+	private Frame addEmptyFrame() {
+		Frame frame;
+		
+		if (frames.size() == 9) {
+			frame = new LastFrame();
+		} else {
+			frame = new Frame();
+		}
+		
+		frames.add(frame);
+		return frame;
+	}
+	
+	
+	/**
+	 * 取得目前最後一個局
+	 */
+	private Frame getCurrentFrame() {
+		
+		if (frames.size() == 0) {
+			addEmptyFrame();
+		}
+		
+		Frame frame = frames.get(frames.size() - 1);
+
+		if (isPlayable() && frame.isOver()) {
+			frame = addEmptyFrame();
+		}
+		
+		return frame;
+	}
+	
+	/**
+	 * 判斷遊戲是否還可繼續玩，繼續丟球
+	 * 如果第10局結束回傳true
+	 */
+	private boolean isPlayable() {
+		if (frames.size() == 10 && frames.get(9).isOver()) {
+			return false;
+		}
+		return true;
 	}
 }
--- src/test/GameTest.java
+++ src/test/GameTest.java
@@ -7,68 +7,216 @@ import org.junit.Test;
 import main.Game;
 
 public class GameTest {
-	
+
 	private Game game;
-	
+
 	@Before
 	public void setup() {
 		game = new Game();
 	}
-	
+
 	@Test
 	public void testRollOne() {
 		int expected = 1;
+
+		game.roll(1);
 		
-		game.roll(1);	
-		game.roll(0);	
-		
+		for (int i = 0; i < 19; i++) {
+			game.roll(0);
+		}
+
 		Assert.assertEquals(expected, game.sum());
 	}
 
 	@Test
 	public void testAllOne() {
 		int expected = 20;
-		
-		for (int i = 0; i < 20; i++){
-			game.roll(1);	//擊倒一瓶
+
+		for (int i = 0; i < 20; i++) {
+			game.roll(1); // 擊倒一瓶
 		}
-				
+
 		Assert.assertEquals(expected, game.sum());
 	}
 	
 	@Test
+	public void testOverRoll() {
+		int expected = 20;
+
+		for (int i = 0; i < 40; i++) {
+			game.roll(1); // 擊倒一瓶
+		}
+
+		Assert.assertEquals(expected, game.sum());
+	}
+
+	@Test
 	public void testOneSpare() {
 		int expected = 14;
-		
+
 		game.roll(2);
 		game.roll(8);
 		game.roll(1);
 		game.roll(2);
-		
-		Assert.assertEquals(expected, game.sum()); 
+
+		Assert.assertEquals(expected, game.sum());
 	}
-	
+
 	@Test
 	public void testOneSpareBonus() {
 		int expected = 14;
-		
+
 		game.roll(1);
 		game.roll(9);
 		game.roll(1);
 		game.roll(2);
-		
-		Assert.assertEquals(expected, game.sum()); 
+
+		Assert.assertEquals(expected, game.sum());
 	}
-	
+
 	@Test
-	public void testOneStike() {
+	public void testOneStrike() {
 		int expected = 16;
 
 		game.roll(10);
 		game.roll(1);
 		game.roll(2);
+
+		Assert.assertEquals(expected, game.sum());
+	}
+	
+	@Test
+	public void testLastFrameOneSpare() {
+		int expected = 11;
+
+		//前九局0分
+		for (int i = 0; i < 18; i++) {
+			game.roll(0);
+		}
+		
+		game.roll(0);
+		game.roll(10);
+		game.roll(1);
 		
 		Assert.assertEquals(expected, game.sum());
 	}
 	
+	@Test
+	public void testLastFrameOneStrike() {
+		int expected = 14;
+
+		//前九局0分
+		for (int i = 0; i < 18; i++) {
+			game.roll(0);
+		}
+		
+		game.roll(10);
+		game.roll(3);
+		game.roll(1);
+		
+		Assert.assertEquals(expected, game.sum());
+	}
+	
+	@Test
+	public void testLastFrameTurkey() {
+		int expected = 30;
+
+		//前九局0分
+		for (int i = 0; i < 18; i++) {
+			game.roll(0);
+		}
+		
+		game.roll(10);
+		game.roll(10);
+		game.roll(10);
+		
+		Assert.assertEquals(expected, game.sum());
+	}
+	
+	@Test
+	public void testOneDouble() {
+		int expected = 53;
+
+		game.roll(10);
+		game.roll(10);
+		game.roll(3);
+		game.roll(7);
+		
+		for (int i = 0; i < 16; i++) {
+			game.roll(0);
+		}
+		
+		Assert.assertEquals(expected, game.sum());
+	}
+	
+	@Test
+	public void testOneTurkey() {
+		int expected = 77;
+
+		game.roll(10);	//10 + 20 = 30, 30
+		game.roll(10);	//10 + 13 = 23, 53
+		game.roll(10);	//10 + 17 = 17, 70
+		game.roll(3);	//3, 73
+		game.roll(4);	//4, 77
+		
+		for (int i = 0; i < 15; i++) {
+			game.roll(0);
+		}
+		
+		Assert.assertEquals(expected, game.sum());
+	}
+	
+	@Test
+	public void testTwoDouble() {
+		int expected = 104;
+
+		game.roll(10);	//10 + 13 = 23, 23
+		game.roll(10);	//10 + 10 = 20, 43
+		game.roll(3);	//3, 46
+		game.roll(7);	//7 + 10 = 17, 63
+		game.roll(10);	//10 + 13 = 23, 86
+		game.roll(10);	//10 + 4 = 14, 100
+		game.roll(3);	//3, 103
+		game.roll(1);	//1, 104
+		
+		for (int i = 0; i < 16; i++) {
+			game.roll(0);
+		}
+		
+		Assert.assertEquals(expected, game.sum());
+	}
+	
+	@Test
+	public void testTowTurkey() {
+		int expected = 151;
+
+		game.roll(10);	//10 + 20 = 30, 30
+		game.roll(10);	//10 + 13 = 23, 53
+		game.roll(10);	//10 + 17 = 17, 70
+		game.roll(3);	//3, 73
+		game.roll(4);	//4, 77
+		game.roll(10);	//10 + 20 = 30, 107
+		game.roll(10);	//10 + 12 = 22, 129
+		game.roll(10);	//10 + 16 = 16, 145
+		game.roll(2);	//2, 147
+		game.roll(4);	//4, 151
+		
+		for (int i = 0; i < 15; i++) {
+			game.roll(0);
+		}
+		
+		Assert.assertEquals(expected, game.sum());
+	}
+	
+	@Test
+	public void testAllStrike() {
+		int expected = 300;
+
+		for (int i = 0; i < 12; i++) {
+			game.roll(10);
+		}
+		
+		Assert.assertEquals(expected, game.sum());
+	}
+
 }
